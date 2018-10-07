1866e13ae383e796b34fb50eddd569c0c390fbc1
Chris Kozlowski <kozlowskicd@gmail.com>
2018-10-06 23:37:46 -0700

    Class constructor complete

--- objects/vehicle-class.js
+++ objects/vehicle-class.js
@@ -1,6 +1,37 @@
 'use strict';
 
-class Car {
+class Vehicle {
+  constructor(name) {
+    this.name = name;
+  }
+  stop() {
+    return 'Stopping...';
+  }
+  drive() {
+    return 'Driving...';
+  }
 }
 
-module.exports = Car;
+class Car extends Vehicle {
+  constructor(name) {
+    super(name);
+    this.wheels = 4;
+  }
+  drive() {
+    super.drive();
+    return `${this.name} drives down the road.`;
+  }
+}
+
+class Motorcycle extends Vehicle {
+  constructor(name) {
+    super(name);
+    this.wheels = 2;
+  }
+  wheelie() {
+    super.drive();
+    return `${this.name} pops a wheelie!`;
+  }
+}
+
+module.exports = {Car, Motorcycle};
--- objects/vehicle-factory.js
+++ objects/vehicle-factory.js
@@ -1,8 +1,15 @@
 'use strict';
 
-function CarFactory() {
+function CarFactory(Vname) {
+  const Vehicle = {
+    drive: () => {console.log('Driving');},
+    stop: () => {console.log('Stopping');},
+    name: Vname,
+  };
+  const Car = Object.assign({
+    wheels: 4}, Vehicle);
+  const Motorcycle = Object.assign({
+    wheels: 2, wheelie: () => {console.log('Wheelie!!');}}, Vehicle);
 }
 
 module.exports = CarFactory;
-
-
